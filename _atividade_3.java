630179ed150df9818ee788a7c46871be539138df
YLucas35 <0001244748@senaimgaluno.com.br>
2026-04-14 10:53:06 -0300

    Create _atividade_3.java

---
+++ _atividade_3.java
@@ -0,0 +1,38 @@
+package Java_Lista_4_Revisao;
+
+import java.util.Scanner;
+
+public class _atividade_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double numb1, numb2, numb3;
+
+        System.out.println("Digite o primeiro lado");
+        numb1 = sc.nextDouble();
+
+        System.out.println("Digite o segundo lado");
+        numb2 = sc.nextDouble();
+
+        System.out.println("Digite o terceiro lado");
+        numb3 = sc.nextDouble();
+
+
+        if ((numb1 + numb2 > numb3) && (numb1 + numb3 > numb2) && (numb2 + numb3 > numb1)) {
+            System.out.println("Você consegue fazer um triângulo com a soma destes lados.");
+
+            if (numb1 == numb2 && numb2 == numb3) {
+                System.out.println("E o seu triângulo é equilátero.");
+            }
+            else if (numb1 == numb2 || numb1 == numb3 || numb2 == numb3) {
+                System.out.println("E o seu triângulo é isósceles.");
+            }
+            else {
+                System.out.println("E o seu triângulo é escaleno.");
+            }
+        }
+        else {
+                System.out.println("Você não consegue fazer um triângulo com a soma destes lados");
+             }
+        sc.close();
+    }
+}
